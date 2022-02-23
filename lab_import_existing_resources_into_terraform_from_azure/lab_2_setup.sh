e6060294ddbe4fa658f87c587e04f48382e4cd8f
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-23 16:22:31 -0500

    Updated lab setup script.

--- lab_import_existing_resources_into_terraform_from_azure/lab_2_setup.sh
+++ lab_import_existing_resources_into_terraform_from_azure/lab_2_setup.sh
@@ -5,4 +5,6 @@ unzip terraform_1.1.6_linux_amd64.zip
 mkdir bin
 mv terraform bin/
 mkdir terraform
-wget https://github.com/ACloudGuru/advanced-terraform-with-azure/blob/main/lab_import_existing_resources_into_terraform_from_azure/providers.tf
+wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_import_existing_resources_into_terraform_from_azure/providers.tf
+mv providers.tf terraform/
+rm -rf terraform_1.1.6_linux_amd64.zip
