911e6bd2662bc858768a78796c22c1772d79df74
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-23 15:35:45 -0500

    Added the lab setup script to the Importing Existing Resources in Azure with Terraform lab.

---
+++ lab_import_existing_resources_into_terraform_from_azure/lab_2_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Lab Setup Script
+wget https://github.com/ACloudGuru/advanced-terraform-with-azure/blob/main/lab_using_terraform_with_azure/terraform_1.1.6_linux_amd64.zip
+unzip terraform_1.1.6_linux_amd64.zip
+mkdir bin
+mv terraform bin/
+mkdir terraform
