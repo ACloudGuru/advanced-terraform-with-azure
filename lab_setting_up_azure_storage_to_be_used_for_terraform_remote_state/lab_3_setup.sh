c29d8b11f91d9f42e7654d46c9d1528a0742222c
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-28 17:21:07 -0500

    Added to the lab_2_setup.sh script.

--- lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/lab_3_setup.sh
+++ lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/lab_3_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # Lab Setup Script
+
+# Variables
+$SUBSCRIPTION_ID = az account show --query id --otsv
+$RESOURCE_GROUP_NAME = az group list --query "[].name" -otsv
+$LOCATION = az group list --query "[].location" -otsv
+
 wget wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_import_existing_resources_into_terraform_from_azure/terraform_1.1.6_linux_amd64.zip
 unzip terraform_1.1.6_linux_amd64.zip
 mkdir bin
@@ -11,3 +17,10 @@ wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/
 mv networking.tf terraform/
 rm -rf terraform_1.1.6_linux_amd64.zip
 rm -rf lab_2_setup.sh
+cd terraform
+terraform init
+sleep 10
+terraform import azurerm_resource_group.guru /subscriptions/${SUBSCRIPTION_ID}/${RESOURCE_GROUP_NAME}
+sleep 5
+echo "name: "$RESOURCE_GROUP_NAME"" >> rg.yml
+echo "location: "$LOCATION"" >> rg.yml
