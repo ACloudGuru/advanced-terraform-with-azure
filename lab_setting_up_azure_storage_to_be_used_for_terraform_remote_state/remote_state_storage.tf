4e8cae099fd4a91f976307d64c30970859a981fd
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-03-01 11:58:42 -0500

    Made some changes to the remote_state_storage.tf file.

--- lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/remote_state_storage.tf
+++ lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/remote_state_storage.tf
@@ -1,9 +1,16 @@
 # Import the Resource Group
 resource "azurerm_resource_group" "guru" {}
 
+# Creates random string for the Unique ID for the Azure Storage Account Name
+resource "random_string" "resource_code" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
 # Creates the Azure Storage Account
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "<STORAGE_ACCOUNT_NAME>"
+  name                     = "<STORAGE_ACCOUNT_NAME>${random_string.resource_code.result}"
   resource_group_name      = azurerm_resource_group.guru.name
   location                 = azurerm_resource_group.guru.location
   account_tier             = "Standard"
