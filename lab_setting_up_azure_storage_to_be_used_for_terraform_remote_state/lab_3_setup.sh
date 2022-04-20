16372f5c7f4fe746e35fe1e034527f3a7c36d446
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-04-20 16:43:35 -0400

    Update lab_3_setup.sh

--- lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/lab_3_setup.sh
+++ lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/lab_3_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Lab Setup Script
 wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/providers.tf
-wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/create-remote-state-storage.tf
+wget https://raw.githubusercontent.com/ACloudGuru/advanced-terraform-with-azure/main/lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/remote_state_storage.tf
 mkdir terraformguru
 mv providers.tf terraformguru/
-mv create-remote-state-storage.tf terraformguru/
+mv remote_state_storage.tf terraformguru/
 rm -rf lab_3_setup.sh
