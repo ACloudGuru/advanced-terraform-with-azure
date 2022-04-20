2ece733c7d1ee95fec9e1901e761b30f9925626d
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-04-20 16:42:41 -0400

    Update remote_state_storage.tf

--- lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/remote_state_storage.tf
+++ lab_setting_up_azure_storage_to_be_used_for_terraform_remote_state/remote_state_storage.tf
@@ -1,9 +1,3 @@
-# Import the Resource Group
-resource "azurerm_resource_group" "guru" {
-  name     = "<RESOURCE_GROUP_NAME>"
-  location = "<LOCATION>"
-}
-
 # Creates random string for the Unique ID for the Azure Storage Account Name
 resource "random_string" "resource_code" {
   length  = 5
@@ -11,23 +5,11 @@ resource "random_string" "resource_code" {
   upper   = false
 }
 
-# Creates the Azure Storage Account
-resource "azurerm_storage_account" "tfstate" {
-  name                     = "<STORAGE_ACCOUNT_NAME>${random_string.resource_code.result}"
-  resource_group_name      = azurerm_resource_group.guru.name
-  location                 = azurerm_resource_group.guru.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  allow_blob_public_access = true
-
-  tags = {
-    environment = "guru"
-  }
-}
-
-# Creates the Azure Storage Container
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "<STORAGE_CONTAINER_NAME>"
-  storage_account_name  = azurerm_storage_account.tfstate.name
-  container_access_type = "blob"
+# Import Resource Group if using ACG's Azure Sandbox
+resource "azurerm_resource_group" "guru" {
+  #name     = "<RESOURCE_GROUP>"
+  #location = "<LOCATION>"
+  #tags     = {
+  #  environment = "demo"
+  #}
 }
